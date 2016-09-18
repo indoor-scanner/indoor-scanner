814e5054a5115905a635f123f3c60fa6b1abd2d3
Martin Jaime <mrtnjaime@gmail.com>
2016-09-18 16:34:53 -0700

    plotting points based on incoming data

--- dataViewer/testServer.js
+++ dataViewer/testServer.js
@@ -9,7 +9,8 @@ server.listen(8000);
 io.on('connection', function (socket) {
   recursiveY(socket);
   recursiveX(socket);
-  addCube(socket, 0);
+  points = genSpherePts(200);
+  addCube(socket, points, 200-1);
 });
 
 var recursiveY = function(socket) {
@@ -26,12 +27,32 @@ var recursiveX = function(socket) {
   }, 25);
 };
 
-var addCube = function(socket, pos) {
-  if (pos < 200) {
-    var newPos = pos % 20;
+var addCube = function(socket, points, index) {
+  if (index > 0) {
+    var newPos = points[index].map(function(point) {return point * 10});
+    //console.log(newPos);
     setTimeout(function() {
       socket.emit('addCube', newPos);
-      addCube(socket, pos+2);
-    }, 0);
+      addCube(socket, points, index-1);
+    }, 100);
   }
+};
+
+var genSpherePts = function(size) {
+  var pts = [];
+  var inc = Math.PI * (3 - Math.sqrt(5));
+  var off = 2.0 / size;
+  var x, y, z, r, phi;
+
+  for(var k = 0; k < size; k++) {
+    y = k * off - 1 + (off / 2);
+    r = Math.sqrt(1 - y * y);
+    phi = k * inc;
+    x = Math.cos(phi) * r;
+    z = Math.sin(phi) * r;
+
+    pts.push([x, y, z])
+  }
+
+  return pts;
 };
