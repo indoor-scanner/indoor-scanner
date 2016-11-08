0ae282ae8cb522a1a1d7ec23272ac73d200ab993
Clint <clinton.bess@galavantier.com>
2016-11-07 16:08:21 -0800

    added pclExamples

---
+++ dataViewer/pclTests/helloNDT/build/CMakeFiles/ndt_demo.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/ndt_demo.dir/hello_ndt.cpp.o"
+  "ndt_demo.pdb"
+  "ndt_demo"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/ndt_demo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
